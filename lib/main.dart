7d1ef81bc9a1ac6b9f43745c337643a9140c10b1
anto <antobuilds@gmail.com>
2025-11-18 13:33:21 +0100

    November 18 class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:w_phonics/pages/home_page.dart';
+import 'package:w_phonics/pages/onboarding_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         )
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: const OnboardingPage(),
     );
   }
 }
---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:w_phonics/widgets/custom_textfield.dart';
+import 'package:w_phonics/widgets/password_textfield.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          children: [
+            Image.asset(
+              "assets/images/mouse_nobg.png",
+              width: 250,
+              height: 250,
+            ),
+            Text(
+              "Welcome to W phonics!",
+              style: Theme.of(
+                context,
+              ).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                spacing: 16,
+                children: [
+                  CustomTextField(label: "Email"),
+                  PassswordTextfield(),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal:24),
+              child: AgreementWIdget(),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: ElevatedButton(onPressed: (){}, child: Text("Next"),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AgreementWIdget extends StatelessWidget {
+  const AgreementWIdget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Checkbox(
+              value: false,
+              onChanged: (value) {},
+              checkColor: Colors.white,
+              activeColor: Colors.white,
+              focusColor: Colors.white,
+            ),
+            Text("SELECT ALL"),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left:24.0),
+          child: Column(children: [Row(
+            children: [
+              Checkbox(
+                value: false,
+                onChanged: (value) {},
+                
+              ),
+              Expanded(child: Text("* I agree to the privacy policy and terms")),
+            ],
+          ),Row(
+            children: [
+              Checkbox(
+                value: false,
+                onChanged: (value) {},
+                checkColor: Colors.white,
+                activeColor: Colors.white,
+                focusColor: Colors.white
+              ),
+              Expanded(child: Text("I want to receive email update from Jolly Learning")),
+            ],
+          ),],),
+        )
+      ],
+    );
+  }
+}
